4ce0499e62b26bf98cc0cf5d82e3a2db83ec4d2d
oana.tibil <oana.tibil@gmail.com>
2019-01-05 15:54:28 +0200

    set-up basic project structure

--- src/main/java/org/Fasttrackit/App.java
+++ src/main/java/org/Fasttrackit/App.java
@@ -9,5 +9,19 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Animal animal = new Cat("Dexter");
+        animal.setAge(2);
+        animal.setFavActivity("running");
+        animal.setFavFood("meat");
+        animal.setHealth(10);
+        animal.setHunger(1);
+        animal.setSpirit(10);
+
+        Petfood petfood = new Petfood();
+        Rescuer rescuer = new Rescuer();
+        rescuer.feedAnimal(animal, petfood);
+
+
+        System.out.println(animal.getName());
     }
 }
